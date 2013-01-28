c29b330de500228a2e77ff53d8f91f19b2c6eb09
Gregory Kuzmicki <gregory.kuzmicki@gmail.com>
2013-01-28 16:19:38 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FocureApp::Application.config.secret_token = '2c9640ada28549ced7fc5f9f80957fc5df07015ea80df343320b7ca14ffd187b633f93a4a63f73684e45c1ce4eab8e3cc05798880c1dca09fd9440209d56d595'
