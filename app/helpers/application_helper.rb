772e346d39c8c1d72d93377cafbcab4d57ea99ef
Gregory Kuzmicki <gregory.kuzmicki@gmail.com>
2013-02-06 19:03:10 -0500

    Change This

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "Everest Juices"
+    base_title = "Everest"
     if page_title.empty?
       base_title
     else
